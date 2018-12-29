fe5be42a7e29c57641a11f6d95a5cf330c889cf8
wudi <156343782@qq.com>
2018-12-29 10:27:37 +0800

    add tips

--- src/pages/index/create-plan.js
+++ src/pages/index/create-plan.js
@@ -21,13 +21,14 @@ class CreatePlan extends Component {
     Taro.navigateTo({ url: '/pages/new-task/new-task' })
   }
   render() {
-    return (
-      <View className="container2">
+    return <View className="container2">
         <AtButton onClick={this.createTask} className="taskBtn" type="primary" size="normal">
           新建一个任务
         </AtButton>
+        <Text className="tips">
+          小tips：&nbsp;&nbsp;&nbsp; 研究表明，当一件事情在第1、3、7、20天重复深度记忆，那么这件事情你一辈子都不会忘记了。 赶紧记录你想要记住的事情吧， 不用谢我 &nbsp;๑乛◡乛๑
+        </Text>
       </View>
-    )
   }
 }
 
